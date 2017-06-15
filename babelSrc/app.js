a175c7afd690c2a149493cf4532eac9a211e6e01
mikesankovich <misankovich@gmail.com>
2017-06-14 21:22:07 -0400

    test

--- babelSrc/app.js
+++ babelSrc/app.js
@@ -22,7 +22,7 @@ bluebird.promisifyAll(mongoose);
 
 const app = express();
 
-mongoose.connect('mongodb://heroku_xs07dgw1:n0t8bbbfg73sjnc7qvif6oedef@ds127962.mlab.com:27962/heroku_xs07dgw1');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:auth/conventionTracker');
 
 // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
